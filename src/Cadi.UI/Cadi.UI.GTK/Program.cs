8ae8648983f1d7c63600d6fb01f3ee7f0a0ba0f1
Duane Newman <duane@alienarc.com>
2019-03-14 15:14:09 -0500

    UI Project Added, GTK wired up

---
+++ src/Cadi.UI/Cadi.UI.GTK/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace Cadi.UI.GTK
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            
+            var app = new App(new GtkInitializer());
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("CADI");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+
+
+    public class GtkInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register any platform specific implementations
+        }
+    }
+}
